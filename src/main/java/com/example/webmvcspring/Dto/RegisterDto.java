1f24c1bfdd99f61a6d2494597f439ba7721613b3
Dandelions16 <doyokbsk@gmail.com>
2023-06-01 02:05:21 +0700

    build(RegisterDTO ):

--- src/main/java/com/example/webmvcspring/Dto/RegisterDto.java
+++ src/main/java/com/example/webmvcspring/Dto/RegisterDto.java
@@ -1,2 +1,23 @@
-package com.example.webmvcspring.Dto;public class RegisterDto {
+package com.example.webmvcspring.Dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+
+@Data
+public class RegisterDto {
+    @NotEmpty(message = "Username not null")
+    private String username;
+    @NotEmpty(message = "Email not null")
+    @Email(message = "Required email format @")
+    private String email;
+    @NotEmpty(message = "Password not null")
+    private String password;
+
 }
