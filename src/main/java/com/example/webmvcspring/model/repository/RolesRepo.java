636bfccd7f6dbec64b74de09a8db1301a9bcc650
Dandelions16 <doyokbsk@gmail.com>
2023-06-01 01:55:13 +0700

    feature(penambahan code):

--- src/main/java/com/example/webmvcspring/model/repository/RolesRepo.java
+++ src/main/java/com/example/webmvcspring/model/repository/RolesRepo.java
@@ -1,2 +1,10 @@
-package com.example.webmvcspring.model.repository;public class RolesRepo {
+package com.example.webmvcspring.model.repository;
+
+import com.example.webmvcspring.model.entity.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RolesRepo extends JpaRepository<Roles,Long> {
+    Roles findByRolesName(String name);
 }
