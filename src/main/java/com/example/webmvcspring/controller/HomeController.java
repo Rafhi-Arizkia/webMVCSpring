a2be685d17d106bc6e4b433b2ede7bcd0d80e38d
Dandelions16 <doyokbsk@gmail.com>
2023-06-01 02:04:42 +0700

    build(SuccessHandler untuk membuat ketika login berhasil akan dilarikan ke halaman masing masing authorities ):

--- src/main/java/com/example/webmvcspring/controller/HomeController.java
+++ src/main/java/com/example/webmvcspring/controller/HomeController.java
@@ -1,2 +1,30 @@
-package com.example.webmvcspring.controller;public class HomeController {
+package com.example.webmvcspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+    @GetMapping("/login")
+    public String showLogin(){
+       return "login";
+    }
+    @GetMapping("/user/index")
+    public String userIndex(Model model){
+        String message = "Hallow Sayang";
+        model.addAttribute("msg",message);
+        return "index";
+    }
+    @GetMapping("/admin/index")
+    public String adminIndex(Model model){
+        String message = "Hallow Admin";
+        model.addAttribute("msg",message);
+        return "adminIndex";
+    }
+    @GetMapping("/denied")
+    public String deniedLogin(){
+        return "denied";
+    }
 }
