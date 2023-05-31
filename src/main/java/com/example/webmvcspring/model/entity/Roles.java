05319129e761b3a5c50d8229d189a793c8d9cab0
Dandelions16 <doyokbsk@gmail.com>
2023-06-01 01:56:25 +0700

    feature(built code):

--- src/main/java/com/example/webmvcspring/model/entity/Roles.java
+++ src/main/java/com/example/webmvcspring/model/entity/Roles.java
@@ -1,2 +1,23 @@
-package com.example.webmvcspring.model.entity;public class Roles {
+package com.example.webmvcspring.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_roles")
+@Data
+@NoArgsConstructor
+public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roles_id;
+    @Column(name = "roles_name")
+    private String rolesName;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<AccountEntity> accountEntities = new ArrayList<>();
 }
