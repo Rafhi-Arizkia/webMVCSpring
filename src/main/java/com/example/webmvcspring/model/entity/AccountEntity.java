8344893db4ce3e7a4d82a9a5ab1b6829d52ef168
Dandelions16 <doyokbsk@gmail.com>
2023-06-01 01:51:03 +0700

    refactor(implement UserDetails):

--- src/main/java/com/example/webmvcspring/model/entity/AccountEntity.java
+++ src/main/java/com/example/webmvcspring/model/entity/AccountEntity.java
@@ -1,2 +1,74 @@
-package com.example.webmvcspring.model.entity;public class AccountEntity {
+package com.example.webmvcspring.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_account")
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+@Builder
+public class AccountEntity implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long account_id;
+    @Column(name = "account_name")
+    private String username;
+    @Column(name = "account_email" ,unique = true)
+    private String email;
+    @Column(name = "account_password")
+    private String password;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "account_roles",
+            joinColumns = @JoinColumn(name = "account_id")
+            , inverseJoinColumns = @JoinColumn(name = "roles_id"))
+    private List<Roles> roles =new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (Roles role : roles){
+            authorities.add(new SimpleGrantedAuthority(role.getRolesName()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
